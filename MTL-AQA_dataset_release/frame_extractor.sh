12b3af4286ce4672baa944bb6896951424191881
Jas Pyneni <jpyneni@gmail.com>
2020-11-09 20:21:36 +0000

    files

--- MTL-AQA_dataset_release/frame_extractor.sh
+++ MTL-AQA_dataset_release/frame_extractor.sh
@@ -1,4 +1,4 @@
-for i in 1 2 3 4 5 6 7 9 10 13 14 17 18 21 22 26;
+for i in 1 2 3 4 5 6 7 9 10 13 14 17 18 22 26;
 do
 mkdir whole_videos_frames/$i
 ffmpeg -i whole_videos/$i.mp4 whole_videos_frames/$i/%06d.jpg
