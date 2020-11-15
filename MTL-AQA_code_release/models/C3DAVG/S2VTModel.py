cadea45be470a7de699d95748dfac396af2a0107
Akhila Ballari <akhila@ballari.com>
2020-11-15 17:47:40 -0500

    Implemented attention for image captioning

--- MTL-AQA_code_release/models/C3DAVG/S2VTModel.py
+++ MTL-AQA_code_release/models/C3DAVG/S2VTModel.py
@@ -26,12 +26,16 @@ class S2VTModel(nn.Module):
     def __init__(self, vocab_size, max_len, dim_hidden, dim_word, dim_vid=2904, sos_id=1, eos_id=0,
                  n_layers=1, rnn_cell='gru', rnn_dropout_p=0.2):
         super(S2VTModel, self).__init__()
+        self.attn = self.Linear(dim_vid, dim_hidden)
         if rnn_cell.lower() == 'lstm':
             self.rnn_cell = nn.LSTM
         elif rnn_cell.lower() == 'gru':
             self.rnn_cell = nn.GRU
-        self.rnn1 = self.rnn_cell(dim_vid, dim_hidden, n_layers,
+        # Changed input dim to dim_hidden to account for attention
+        self.rnn1 = self.rnn_cell(dim_hidden, dim_hidden, n_layers,
                                   batch_first=True, dropout=rnn_dropout_p)
+        # self.rnn1 = self.rnn_cell(dim_vid, dim_hidden, n_layers,
+        #                           batch_first=True, dropout=rnn_dropout_p)
         self.rnn2 = self.rnn_cell(dim_hidden + dim_word, dim_hidden, n_layers,
                                   batch_first=True, dropout=rnn_dropout_p)
         self.dim_vid = dim_vid
@@ -48,11 +52,18 @@ class S2VTModel(nn.Module):
     def forward(self, vid_feats, target_variable=None, mode='train', opt={}):
         batch_size, n_frames, _ = vid_feats.shape
 
+        # Passing through attention
+        attention = self.attn(vid_feats)
+
         padding_words = Variable(vid_feats.data.new(batch_size, n_frames, self.dim_word)).zero_()
         padding_frames = Variable(vid_feats.data.new(batch_size, 1, self.dim_vid)).zero_()
         state1 = None
         state2 = None
-        output1, state1 = self.rnn1(vid_feats, state1)
+
+        # Pass attention through rnn1
+        output1, state1 = self.rnn1(attention, state1)
+
+        # output1, state1 = self.rnn1(vid_feats, state1)
         input2 = torch.cat((output1, padding_words), dim=2)
         output2, state2 = self.rnn2(input2, state2)
 
@@ -62,7 +73,10 @@ class S2VTModel(nn.Module):
             for i in range(self.max_length - 1):
                 target_variable = target_variable.type(torch.LongTensor).cuda()
                 current_words = self.embedding(target_variable[:, i])
-                output1, state1 = self.rnn1(padding_frames, state1)
+                attention = self.attn(padding_frames)
+                # Pass through attention
+                output1, state1 = self.rnn1(attention, state1)
+                # output1, state1 = self.rnn1(padding_frames, state1)
                 input2 = torch.cat((output1, current_words.unsqueeze(1)), dim=2)
                 output2, state2 = self.rnn2(input2, state2)
                 logits = self.out(output2.squeeze(1))
@@ -74,7 +88,10 @@ class S2VTModel(nn.Module):
             for i in range(self.max_length - 1):
                 self.rnn1.flatten_parameters()
                 self.rnn2.flatten_parameters()
-                output1, state1 = self.rnn1(padding_frames, state1)
+                # Pass through attention
+                attention = self.attn(padding_frames)
+                output1, state1 = self.rnn1(attention, state1)
+                # output1, state1 = self.rnn1(padding_frames, state1)
                 input2 = torch.cat(
                     (output1, current_words.unsqueeze(1)), dim=2)
                 output2, state2 = self.rnn2(input2, state2)
