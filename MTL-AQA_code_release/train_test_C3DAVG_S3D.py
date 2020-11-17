c5ebbd4a1b4b6f0c23045c3507191e3e0ac5c629
Akhila Ballari <akhila@ballari.com>
2020-11-16 22:09:29 -0500

    Made changes to run s3d + attention

--- MTL-AQA_code_release/train_test_C3DAVG_S3D.py
+++ MTL-AQA_code_release/train_test_C3DAVG_S3D.py
@@ -31,10 +31,12 @@ torch.manual_seed(randomseed); torch.cuda.manual_seed_all(randomseed); random.se
 torch.backends.cudnn.deterministic=True
 
 current_run = 1 # CHANGE THIS FOR LOG FILE (BEFORE RUNNING HAPPENS)
-train_logging_file_name = "train_logging_file_" + str(current_run) + ".txt"
+# train_logging_file_name = "train_logging_file_" + str(current_run) + ".txt"
+train_logging_file_name = "s3d_attn_train_logging_file_" + str(current_run) + ".txt"
 train_logging_file = open(train_logging_file_name,"x")
 train_logging_file.close()
-test_logging_file_name = "test_logging_file_" + str(current_run) + ".txt"
+# test_logging_file_name = "test_logging_file_" + str(current_run) + ".txt"
+test_logging_file_name = "s3d_attn_test_logging_file_" + str(current_run) + ".txt"
 test_logging_file = open(test_logging_file_name, "x")
 test_logging_file.close()
 
@@ -256,7 +258,8 @@ def main():
 
     # actual training, testing loops
     for epoch in range(100):
-        saving_dir = 'c3davg_140_saved_s3d' # ADDED PATH FOR SAVING DIRECTORY
+        # saving_dir = 'c3davg_140_saved_s3d' # ADDED PATH FOR SAVING DIRECTORY
+        saving_dir = 'c3davg_140_saved_s3d_attn' # ADDED PATH FOR SAVING DIRECTORY
         print('-------------------------------------------------------------------------------------------------------')
         for param_group in optimizer.param_groups:
             print('Current learning rate: ', param_group['lr'])
@@ -310,7 +313,8 @@ if __name__ == '__main__':
         model_caption = S2VTModel(vocab_size, max_cap_len, caption_lstm_dim_hidden,
                                   caption_lstm_dim_word, caption_lstm_dim_vid,
                                   rnn_cell=caption_lstm_cell_type, n_layers=caption_lstm_num_layers,
-                                  rnn_dropout_p=caption_lstm_dropout)
+                                  rnn_dropout_p=caption_lstm_dropout,
+                                  use_attention=True)
         model_caption = model_caption.cuda()
         print('Using Captioning Loss')
 
